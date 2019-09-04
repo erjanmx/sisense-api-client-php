2df726136dcd8c99cd690edbce891dc2a0e6f909
erjanmx <erjanmx@gmail.com>
2019-09-04 23:51:48 +0200

    Add v0.9 Authorization

---
+++ tests/Api/v09/AuthorizationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sisense\Tests\V09;
+
+use Sisense\Client;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * Class AuthorizationTest
+ */
+class AuthorizationTest extends TestCase
+{
+    /**
+     * @var MockObject|Client
+     */
+    protected $clientMock;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->clientMock = $this->createPartialMock(Client::class, ['runRequest']);
+
+        $this->clientMock->useVersion('v0.9');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\Authorization::login
+     */
+    public function testIsAuth()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('auth/isauth', 'GET')
+            ->willReturn([]);
+
+        $this->clientMock->authorization->isauth();
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\Authorization::logout
+     */
+    public function testLogout()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('auth/logout', 'GET')
+            ->willReturn([]);
+
+        $this->clientMock->authorization->logout();
+    }
+}
