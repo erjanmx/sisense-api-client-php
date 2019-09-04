d9772348d97c2697983cee125457ba70e5653679
erjanmx <erjanmx@gmail.com>
2019-09-04 16:17:03 +0200

    Update docs

--- src/Api/Users.php
+++ src/Api/Users.php
@@ -19,29 +19,25 @@ class Users extends AbstractApi
      * The expandable fields for the user object are groups, adgroups and role.
      *
      * @param  array $parameters Array containing the necessary params.
-     *                           $parameters = [
-     *                           'email'     => (string) Email to filter by.
-     *                           'firstName' => (string) First name to filter by.
-     *                           'lastName'  => (string) Last name to filter by.
-     *                           'roleId'    => (string) Role ID to filter by.
-     *                           'groupId'   => (string) Group ID to filter by.
-     *                           'active'    => (bool) User state to filter by - true for active users, false for
-     *                           inactive users.
-     *                           'origin'    => (string) User origin to filter by - ad for active directory or sisense.
-     *                           'ids'       => (array) Array of user IDs to get, separated by a comma (,) and without
-     *                           spaces. ???
-     *                           'fields'    => (string) Whitelist of fields to return for each document. Can also
-     *                           define which fields to exclude by prefixing field names with "-".
-     *                           'sort'      => (string) Field by which the results should be sorted.
-     *                           Ascending by default, descending if prefixed by "-".
-     *                           'skip'      => (int) Number of results to skip from the start of the data set.
-     *                           To be used with the limit parameter for paging.
-     *                           'limit'     => (int) How many results should be returned.
-     *                           To be used with the skip parameter for paging.
-     *                           'expand'    => (string) List of fields that should be expanded
-     *                           (substitutes their IDs with actual objects).
-     *                           May be nested using the "resource.subResource" format.
-     *                           ]
+     * $parameters = [
+     *      'email'     => (string) Email to filter by.
+     *      'firstName' => (string) First name to filter by.
+     *      'lastName'  => (string) Last name to filter by.
+     *      'roleId'    => (string) Role ID to filter by.
+     *      'groupId'   => (string) Group ID to filter by.
+     *      'active'    => (bool)   User state to filter by - true for active users, false for inactive users.
+     *      'origin'    => (string) User origin to filter by - ad for active directory or sisense.
+     *      'ids'       => (array)  Array of user IDs to get, separated by a comma (,) and without spaces. ???
+     *      'fields'    => (string) Whitelist of fields to return for each document.
+     *                              Can also define which fields to exclude by prefixing field names with "-".
+     *      'sort'      => (string) Field by which the results should be sorted.
+     *                              Ascending by default, descending if prefixed by "-".
+     *      'skip'      => (int)    Number of results to skip from the start of the data set.
+     *                              To be used with the limit parameter for paging.
+     *      'limit'     => (int)    How many results should be returned. To be used with the skip parameter for paging.
+     *      'expand'    => (string) List of fields that should be expanded (substitutes their IDs with actual objects).
+     *                              May be nested using the "resource.subResource" format.
+     *  ]
      *
      * @return array
      * @throws GuzzleException
